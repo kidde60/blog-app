7652bea32a037b6b8cd1ba76dfa71faf3bfb09e2
kidde60 <kgeorgewilliam60@gmail.com>
2022-12-10 14:53:27 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -40,8 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_143906) do
     t.integer "comments_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "users_id"
-    t.index ["users_id"], name: "index_posts_on_users_id"
+  t.bigint "author_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
