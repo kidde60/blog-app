876f4b5cac8abd4183a5eef41de5ab0ae9687450
kidde60 <kgeorgewilliam60@gmail.com>
2022-12-16 00:42:04 +0300

    Add likes tests

--- spec/like_spec.rb
+++ spec/like_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe Like, type: :model do
   context 'test likes' do
     before :each do
-      @user = User.create(name: 'Jennie', photo: 'https://unsplash.com/photos/Th-i7Z1ufK8', bio: 'Artist')
-      @post = Post.create(author: @user, title: 'Cafe', text: 'My fav place')
-      @comment = Comment.create(post: @post, author: @user, text: 'Love it!')
+      @user = User.create(name: 'william', photo: 'https://github.com/kidde60', bio: 'web developer')
+      @post = Post.create(author: @user, title: 'Title', text: 'My sample text')
+      @comment = Comment.create(post: @post, author: @user, text: 'Awesome!')
     end
 
     it 'comments author should equal user who made the comment' do
