0e875198e353b1c4ab69ba864b4113c061fea2dc
kidde60 <kgeorgewilliam60@gmail.com>
2022-12-21 20:54:13 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: "users#index"
-  get 'users/:id' => 'users#show', as: "user"
-  get 'users/:author_id/posts' => 'posts#index', as: "user_posts"
-  get 'users/:author_id/posts/:id' => 'posts#show', as: "user_post"
+  root'users#index'
+  resources :users, only: [:index, :show] do
+  resources :posts, only: [:index, :show]
+end
 end
