a659057709ac6dd6da94454fe464d665358200b4
kidde60 <kgeorgewilliam60@gmail.com>
2022-12-22 10:18:45 +0300

    Add posts tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET/index' do
+    it 'Check if response status is correct' do
+      get '/users/:id/posts'
+      expect(response).to have_http_status(:ok)
+    end
     it 'should pass with status 200' do
       get '/users/120/posts'
       expect(response.status).to eql(200)
@@ -14,23 +18,27 @@ RSpec.describe 'Posts', type: :request do
 
     it 'should include correct text' do
       get '/users/120/posts'
-      expect(response.body).to include('List of posts by given user')
+      expect(response.body).to include('List of all posts by a user ')
     end
   end
 
   describe 'GET/show' do
+    it 'Check if response status is correct' do
+      get '/users/:id/posts/:post_id'
+      expect(response).to have_http_status(:ok)
+    end
     it 'should pass with status 200' do
       get '/users/120/posts/20'
       expect(response.status).to eql(200)
     end
 
     it 'should render correct template' do
-      get '/users/120/posts/20'
+      get '/users/120/posts/2'
       expect(response).to render_template(:show)
     end
 
     it 'should include correct text' do
-      get '/users/120/posts/20'
+      get '/users/120/posts/2'
       expect(response.body).to include('List of post by given id')
     end
   end
