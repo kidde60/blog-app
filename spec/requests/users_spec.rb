ee3e2968a19d1b7fb7a1713120d0406520ccd8c5
kidde60 <kgeorgewilliam60@gmail.com>
2022-12-22 10:18:23 +0300

    Add users tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET/index' do
+    it 'Check if response status is correct' do
+      get '/'
+      expect(response).to have_http_status(:ok)
+    end
     it 'should pass with status 200' do
       get '/'
       expect(response.status).to eql(200)
@@ -14,23 +18,27 @@ RSpec.describe 'Users', type: :request do
 
     it 'should include correct text' do
       get '/'
-      expect(response.body).to include('List of users')
+      expect(response.body).to include('List of all users')
     end
   end
 
   describe 'GET/show' do
+    it 'Check if response status is correct' do
+      get '/users/:id'
+      expect(response).to have_http_status(:ok)
+    end
     it 'should pass with status 200' do
-      get '/users/12'
+      get '/users/1'
       expect(response.status).to eql(200)
     end
 
     it 'should render correct template' do
-      get '/users/12'
+      get '/users/1'
       expect(response).to render_template(:show)
     end
 
     it 'should include correct text' do
-      get '/users/12'
+      get '/users/1'
       expect(response.body).to include('List of user by given id')
     end
   end
