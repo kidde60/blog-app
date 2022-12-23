a91ae3bc1582227ac2e9962340ecc78992148557
kidde60 <kgeorgewilliam60@gmail.com>
2022-12-23 19:01:45 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
   resources :posts, only: [:index, :show]
 end
+get 'users/:author_id/posts/:id/comments/new' => 'comments#new', as: "comments_new"
+post 'users/:author_id/posts/:id/comments' => 'comments#create', as: "comment"
+
+# will match a GET request to the given URL and send it to the create action in the LikesController.
+get 'users/:author_id/posts/:id/likes/new' => 'likes#create', as: "likes_create"
+
 end
